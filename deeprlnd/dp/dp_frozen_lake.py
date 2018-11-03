3553b42d483c8f702b256849218c733abbdf373e
wpumacay <wpumacay@gmail.com>
2018-11-03 17:58:06 -0500

    [Update]: updated dp sample with q-policy evaluation

--- deeprlnd/dp/dp_frozen_lake.py
+++ deeprlnd/dp/dp_frozen_lake.py
@@ -26,18 +26,18 @@ print( 'PART1: Iterative Policy Evaluation ***************' )
 :param gamma  : Discount factor
 """
 def bellman_expectation_backup( state, V, env, policy, gamma ) :
-    _V = 0.0
+    _v = 0.0
     # first part (exp. over action in policy)
     for a in range( env.nA ) :
         _pi_a = policy[state][a]
         # second part (exp. over next state and reward)
         _transitions = env.P[state][a]
         for ( _ptransition, _nextS, _reward, _done ) in _transitions :
-            _V += _pi_a * _ptransition * ( _reward + gamma * V[_nextS] )
-    return _V
+            _v += _pi_a * _ptransition * ( _reward + gamma * V[_nextS] )
+    return _v
 
 
-def policy_evaluation(env, policy, gamma=1, theta=1e-8):
+def policy_evaluation( env, policy, gamma=1, theta=1e-8 ):
     V = np.zeros(env.nS)
     
     while True :
@@ -109,4 +109,48 @@ print( np.array( [[ 0.0147094  , 0.01393978 , 0.01393978 , 0.01317015],
                   [ 0.25238823 , 0.53837051 , 0.52711478 , 0.43929118],
                   [ 0.         , 0.         , 0.         , 0.        ]] ) )
 
-print( 'END PART 1 ***************************************' )
+# Compute Q(s,a) directly
+
+def q_bellman_expectation_backup( state, action, Q, env, policy, gamma ) :
+    q = 0.0
+    # extract one-step dynamics for this state-action
+    _transitions = env.P[state][action]
+    # expectation over one-step dynamics
+    for _ptransition, _nextS, _reward, _done in _transitions :
+        _v = 0.0
+        for a in range( env.nA ) :
+            _paction = policy[_nextS][a]
+            _v += _paction * Q[_nextS][a]
+
+        q += _ptransition * ( _reward + gamma * _v )
+
+    return q
+
+def q_policy_evaluation( env, policy, gamma = 1, theta = 1e-8 ) :
+    Q = np.zeros( [ env.nS, env.nA ] )
+
+    while True :
+        _error = 0.0
+
+        for s in range( env.nS ) :
+            for a in range( env.nA ) :
+                # cache old value
+                _qOld = Q[s][a]
+                # do bellman backup
+                Q[s][a] = q_bellman_expectation_backup( s, a, Q, env, policy, gamma )
+                # current value
+                _qNew = Q[s][a]
+                # compute bellman error
+                _error = max( _error, np.abs( _qNew - _qOld ) )
+            
+        if _error < theta :
+            print( 'Converged' )
+            break
+    
+    return Q
+
+Q_bellman = q_policy_evaluation( _envWrapper.env, _random_policy )
+print( 'Q from bellman' )
+print( Q_bellman )
+
+print( 'END PART 2 ***************************************' )
